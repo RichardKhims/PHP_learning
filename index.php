20b02bc2457e832bf3c4eecfd6427571781f3770
Richard Khims <khims.richard@gmail.com>
2019-07-20 00:47:00 +0400

    add output results into csv files

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
         <meta charset="utf-8">
     </head>
     <body>
-        <form action="index.php" method="post">
-            Введите текст: <input type="text" name="text" size="200"><br/>
+        <form action="" method="post" enctype="multipart/form-data">
+            <textarea name="text" cols="150" rows="20" placeholder="Введите текст"></textarea>><br/>
+            Укажите файл: <input type="file" name="file"><br/>
             <input type="submit" value="Ok">
         </form>
     </body>
@@ -22,29 +23,51 @@ function getWordsFromString($string) {
     return array();
 }
 
-$text = $_POST['text'] ?? '';
+function getResult($words)
+{
+    $result = array();
+    foreach ($words as $word) {
+        if (!array_key_exists($word, $result)) $result[$word] = 1;
+        else $result[$word]++;
+    }
 
-if (empty($text)) die("Исходный текст пуст");
-
-$words = getWordsFromString($text);
-
-$result = array();
-
-foreach ($words as $word) {
-    if (!array_key_exists($word, $result)) $result[$word] = 1;
-    else $result[$word]++;
+    return $result;
 }
 
-echo "<p>Исходный текст: {$text}</p>".PHP_EOL;
-echo "<p>Результат</p>".PHP_EOL;
+function saveResult($result, $words_count, $uniq_words_count, $filename) {
+    $file = fopen($filename, "w");
 
-foreach ($result as $key => $count) {
-    echo "{$key}:{$count}<br>".PHP_EOL;
+    fwrite($file, "Кол-во слов;{$words_count}\n");
+    fwrite($file, "Кол-во уникальных слов;{$uniq_words_count}\n");
+    foreach ($result as $key => $count) {
+        fwrite($file, "$key;{$count}\n");
+    }
+
+    fclose($file);
 }
 
-$words_count = count($words);
-$uniq_words_count = count($result);
+function makeCsvFromText($text, $filename) {
+    if (empty($text)) return '';
 
-echo "<p>Всего слов в тексте: {$words_count}</p>".PHP_EOL;
-echo "<p>Уникальных слов в тексте: {$uniq_words_count}</p>".PHP_EOL;
+    $words = getWordsFromString($text);
+    $result = getResult($words);
+
+    $words_count = count($words);
+    $uniq_words_count = count($result);
+
+    saveResult($result, $words_count, $uniq_words_count, $filename);
+
+    return $filename;
+}
+
+echo "<br>";
+$out_file = makeCsvFromText($_POST['text'] ?? '', 'from_text.csv');
+if (!empty($out_file)) echo "<a href='{$out_file}'>Результат 1</a>";
+
+echo "<br>";
+$file = $_FILES['file']['tmp_name'] ?? '';
+if (file_exists($file)) {
+    $out_file = makeCsvFromText(file_get_contents($_FILES['file']['tmp_name']) ?? '', 'from_file.csv');
+    if (!empty($out_file)) echo "<a href='{$out_file}'>Результат 2</a>";
+}
 ?>
